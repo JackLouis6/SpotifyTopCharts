1426d0c7df5248269a522265b95d7676b34656cd
Jack Louis <jbl5925@gmail.com>
2022-02-03 10:55:19 -0600

    Initial Commit

---
+++ SpotifyTopCharts/SpotifyTopChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyTopChartsApp.swift
+//  SpotifyTopCharts
+//
+//  Created by Student on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyTopChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
