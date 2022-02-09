a3c499fa091267b5a7997e59e93a81a2cb21add2
Jack Louis <jbl5925@gmail.com>
2022-02-09 10:52:35 -0600

    Added JSON features, API calling, and basic labeling

--- SpotifyTopCharts/ContentView.swift
+++ SpotifyTopCharts/ContentView.swift
@@ -8,49 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var songs = [Song]()
-    
+    @State private var showingAlert = false
     var body: some View {
-     
         NavigationView {
-                    List(songs) { song in
-                        NavigationLink(
-                            destination: Text(song.position)
-                                .padding(),
-                            label: {
-                                Text(song.track_title)
-                                Text(song.artists)
-                                Text(song.streams)
-                            })
-                    }
-                    .navigationTitle("Spotify Top Charts")
-                }
+            List(songs) { song in
+                NavigationLink(
+                    destination: Text(song.track_url)
+                        .padding(),
+                    label: {
+                        Text(song.position); Text("-")
+                        Text(song.track_title)
+                        Text("Streams:"); Text(song.streams)
+                    })
+            }
+            .navigationTitle("Spotify Top Charts")
+        }
         .onAppear(perform: {
-                    getSongs()
-                })
+            getSongs()
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
+        })
     }
     
     func getSongs() {
-            let apiKey = "rapidapi-key=11e76f41famsh4a8f4c04d83db94p14e066jsndbc159a476f9"
-            let query = "https://spotfiy-charts.p.rapidapi.com/?type=regional&country=global&recurrence=daily&date=latest&\(apiKey)"
-            if let url = URL(string: query) {
-                if let data = try? Data(contentsOf: url) {
-                    let json = try! JSON(data: data)
-                    if json["success"] == true {
-                        let contents = json["body"].arrayValue
-                        for item in contents {
-                            let position = item["sposition"].stringValue
-                            let track_title = item["track_title"].stringValue
-                            let artists = item["artists"].stringValue
-                            let streams = item["streams"].stringValue
-                            let song = Song(position: position, track_title: track_title, artists: artists, streams: streams)
-                            songs.append(song)
-                        }
+        let apiKey = "rapidapi-key=11e76f41famsh4a8f4c04d83db94p14e066jsndbc159a476f9"
+        let query = "https://spotfiy-charts.p.rapidapi.com/?type=regional&country=global&recurrence=daily&date=latest&\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["response"] == "success" {
+                    let contents = json["content"].arrayValue
+                    for item in contents {
+                        let position = item["position"].stringValue
+                        let track_title = item["track_title"].stringValue
+                        let track_url = item["track_url"].stringValue
+                        let streams = item["streams"].stringValue
+                        let song = Song(position: position, track_title: track_title, track_url: track_url, streams: streams)
+                        songs.append(song)
                     }
+                    return
                 }
             }
         }
+        showingAlert = true
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -63,7 +68,7 @@ struct Song: Identifiable {
     let id = UUID()
     var position: String
     var track_title: String
-    var artists: String
+    var track_url: String
     var streams: String
     
 }
